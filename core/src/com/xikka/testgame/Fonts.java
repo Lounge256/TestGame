4f11c2487e038a149d18dedf999162b3d6a2cc4f
srpatel <sunilpatel@Sunils-iMac.home>
2014-11-19 21:57:59 +0000

    Add a font
    We can now use Fonts.smallFont.draw(…). We can pick a new font later on…

---
+++ core/src/com/xikka/testgame/Fonts.java
@@ -0,0 +1,19 @@
+package com.xikka.testgame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+public class Fonts {
+	public static BitmapFont smallFont;
+	public static void init(){
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("pixelfont.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+		parameter.size = 32;
+
+		smallFont = generator.generateFont(parameter);
+		generator.dispose();
+	}
+}
+
