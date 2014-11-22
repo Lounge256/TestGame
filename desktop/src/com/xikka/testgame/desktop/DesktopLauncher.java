eec1dbc0661677784a0841e9403d353fe624c600
srpatel <sunilpatel@Sunils-iMac.home>
2014-11-22 20:00:15 +0000

    Change desktop screen size to match mobile
    Using iPhone 5 aspect ratio.

--- desktop/src/com/xikka/testgame/desktop/DesktopLauncher.java
+++ desktop/src/com/xikka/testgame/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.xikka.testgame.TestGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		// Match the ~16:9 aspect ratio on iPhone 5
+		// Should work with any screen-size, but may as well match roughly mobile dimensions
+		config.width = 320;
+		config.height = 568;
 		new LwjglApplication(new TestGame(), config);
 	}
 }
